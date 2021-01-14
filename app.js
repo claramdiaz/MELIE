63409486f50c1bbc55feb8504b780ad33f5c78c1
claramdiaz <claramdiaz@hotmail.com>
2021-01-14 20:48:18 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express= require("express");
+const app= express ();
+
+app.listen (3000,()=> console.log("Servidor inicializado"));
+
+const path =require ("path");
+
+app.use(express.static(path.resolve(__dirname,"public")));
+
+
+
+// Se definen rutas y respuestas en el mismo script (recordar que esto puede modularizarse)
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
