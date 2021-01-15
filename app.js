e5b25fd552d922bfb27ae4fe6a224a2c9328e8ba
claramdiaz <claramdiaz@hotmail.com>
2021-01-15 19:12:53 -0300

    .

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express= require("express");
 const app= express ();
 
-app.listen (3000,()=> console.log("Servidor inicializado"));
+app.listen (process.env.PORT || 3000,()=> console.log("Servidor inicializado"));
 
 const path =require ("path");
 
